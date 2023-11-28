577d0f828044c3ed40d2b3f84b5aca616089ec56
Leohafs <leohafs2903@gmail.com>
2023-11-29 04:02:53 +0700

    update

---
+++ app/src/main/java/com/example/appdua/DetailAlamatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appdua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailAlamatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_alamat)
+    }
+}
