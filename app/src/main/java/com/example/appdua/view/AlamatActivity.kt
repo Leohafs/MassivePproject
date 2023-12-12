b3aeb10fa9fb11342c2d25216d8774f5e2efc34c
Leohafs <leohafs2903@gmail.com>
2023-12-12 07:34:21 +0700

    update

--- app/src/main/java/com/example/appdua/view/AlamatActivity.kt
+++ app/src/main/java/com/example/appdua/view/AlamatActivity.kt
@@ -4,13 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appdua.R
 
 class AlamatActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var nama: EditText
+    private lateinit var namaJalan: EditText
+    private lateinit var rt: EditText
+    private lateinit var rw: EditText
+    private lateinit var info: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alamat)
+
+        nama = findViewById(R.id.edt_nama)
+        namaJalan = findViewById(R.id.edt_namajalan)
+        rt = findViewById(R.id.edt_rt)
+        rw = findViewById(R.id.edt_rw)
+        info = findViewById(R.id.edt_infolainnya)
 
         val btnSimpan: Button = findViewById(R.id.btn_simpan)
         btnSimpan.setOnClickListener(this)
@@ -19,7 +32,16 @@ class AlamatActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when(v.id){
             R.id.btn_simpan -> {
+
+                val bundle = Bundle()
+                bundle.putString("nama",nama.text.toString())
+                bundle.putString("namaJalan",namaJalan.text.toString())
+                bundle.putString("rt",rt.text.toString())
+                bundle.putString("rw",rw.text.toString())
+                bundle.putString("info",info.text.toString())
+
                 val intent = Intent(this@AlamatActivity, DetailAlamatActivity::class.java)
+                intent.putExtras(bundle)
                 startActivity(intent)
             }
         }
--- app/src/main/java/com/example/appdua/view/DetailAlamatActivity.kt
+++ app/src/main/java/com/example/appdua/view/DetailAlamatActivity.kt
@@ -1,12 +1,27 @@
 package com.example.appdua.view
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appdua.R
 
 class DetailAlamatActivity : AppCompatActivity() {
+
+    private lateinit var alamatPengiriman: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_alamat)
+
+        alamatPengiriman = findViewById(R.id.alamat_pengiriman)
+
+        val bundle = intent.extras
+        if(bundle != null){
+            val nama = bundle.getString("nama")
+            val namaJalan = bundle.getString("namaJalan")
+            val rt = bundle.getString("rt")
+            val rw = bundle.getString("rw")
+            val info = bundle.getString("info")
+            alamatPengiriman.text = "$nama,$namaJalan,Rt.$rt,/ Rw.$rw,$info"
+        }
     }
 }
